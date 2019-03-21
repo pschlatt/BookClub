4dfbd618cb8a3b1a6597661c19fb2e5e3a44b6fb
Erin King <44042415+erin-king@users.noreply.github.com>
2019-03-20 19:57:06 -0600

    adds migration to add_cover_to_books_

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190320194748) do
+ActiveRecord::Schema.define(version: 20190321015444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20190320194748) do
     t.string "title"
     t.integer "number_of_pages"
     t.integer "publish_year"
+    t.string "cover"
   end
 
   create_table "reviews", force: :cascade do |t|
