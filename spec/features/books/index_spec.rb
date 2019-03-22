464daef233715726646425deac4da858c7f524c1
Erin King <44042415+erin-king@users.noreply.github.com>
2019-03-22 09:57:19 -0600

    add test for reviews

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -10,17 +10,48 @@ RSpec.describe "user_index", type: :feature do
     @author_3 = @book_3.authors.create(name: "James Joyce")
     @book_4 = Book.create(title: "The Talisman", number_of_pages: 921, publish_year: 1984, cover: "https://images-na.ssl-images-amazon.com/images/I/81muDiETxIL.jpg")
     @author_4 = @book_4.authors.create(name: "Peter Straub, Stephen King")
+
+    @review_1 = @book_1.reviews.create(title: "Review Title 1", rating: 1, review_text: "This is the 1st review.", username: "UserYou1")
+    @review_2 = @book_1.reviews.create(title: "Review Title 2", rating: 3, review_text: "This is the 2nd review.", username: "UserYou2")
+    @review_3 = @book_1.reviews.create(title: "Review Title 3", rating: 4, review_text: "This is the 3rd review.", username: "UserYou3")
+
+    @review_1 = @book_2.reviews.create(title: "Review Title 1", rating: 1, review_text: "This is the 1st review.", username: "UserYou1")
+    @review_2 = @book_2.reviews.create(title: "Review Title 2", rating: 3, review_text: "This is the 2nd review.", username: "UserYou2")
+    @review_3 = @book_2.reviews.create(title: "Review Title 3", rating: 4, review_text: "This is the 3rd review.", username: "UserYou3")
+
+
+
   end
 
-  it "user_can_see_all_books" do
+  it 'user_can_see_all_books' do
 
     visit books_path
 
-    expect(page).to have_content(@book_1.title)
-    expect(page).to have_content(@book_1.number_of_pages)
-    expect(page).to have_content(@book_1.authors.first.name)
-    expect(page).to have_content(@book_1.publish_year)
-    expect(page).to have_css("img[src*='#{@book_1.cover}']")
-    expect(page).to have_content(@book_4.authors.first.name)
+    within "#book-#{@book_1.id}"
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content(@book_1.number_of_pages)
+
+    end
   end
+
+  it 'user can see average book rating and total number of reviews' do
+
+    visit books_path
+
+    within "#book-#{@book_2.id}"
+      expect(page).to have_content("Average Rating: #{@book_2.average_rating}")
+      expect(page).to have_content("Total Reviews: #{@book_2.total_reviews}")
+
+    end
+
+    within "#book-#{@book_1.id}"
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content(@book_1.number_of_pages)
+      expect(page).to have_content(@book_1.authors.first.name)
+      expect(page).to have_content(@book_1.publish_year)
+      expect(page).to have_css("img[src*='#{@book_1.cover}']")
+    end
+
+  end
+
 end
