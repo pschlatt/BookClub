7bd6d26f99df534261b591602a87fda12c7bad65
Erin King <44042415+erin-king@users.noreply.github.com>
2019-03-25 16:53:48 -0600

    Add author show page without multiple author functionality

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe "author showpage", type: :feature do
   before :each do
     @author = Author.create(name: "Erin King")
     @book_1 = @author.books.create(title: "Book_1", number_of_pages: 5, publish_year: 1900, cover: "https://images.penguinrandomhouse.com/cover/9780679645689")
-    @book_2 = @author.books.create(title: "Book_2", number_of_pages: 10, publish_year: 1910, cover: "https://images-na.ssl-images-amazon.com/images/I/81muDiETxIL.jpg")
-    @book_3 = @author.books.create(title: "Book_3", number_of_pages: 50, publish_year: 1920, cover: "https://images.penguinrandomhouse.com/cover/9780679645689")
-    @book_4 = @author.books.create(title: "Book_4", number_of_pages: 500, publish_year: 1930, cover: "https://images-na.ssl-images-amazon.com/images/I/81muDiETxIL.jpg")
-    @book_5 = @author.books.create(title: "Book_5", number_of_pages: 100, publish_year: 1940, cover: "https://images.penguinrandomhouse.com/cover/9780679645689")
+    @book_2 = @author.books.create(title: "Book_2", number_of_pages: 10, publish_year: 1910, cover: "https://i.pinimg.com/originals/2e/42/d2/2e42d25ee87ad6bb5f512bd86e099233.jpg")
+    @book_3 = @author.books.create(title: "Book_3", number_of_pages: 50, publish_year: 1920, cover: "https://images-na.ssl-images-amazon.com/images/I/51wTLf4JVwL._SX384_BO1,204,203,200_.jpg")
+    @book_4 = @author.books.create(title: "Book_4", number_of_pages: 500, publish_year: 1930, cover: "https://images-na.ssl-images-amazon.com/images/I/51vv75oglyL._SX326_BO1,204,203,200_.jpg")
+    @book_5 = @author.books.create(title: "Book_5", number_of_pages: 100, publish_year: 1940, cover: "https://images-na.ssl-images-amazon.com/images/I/81vjde0JFHL.jpg")
   end
 
   it "shows a page with author information" do
 
     visit author_path(@author)
 
-    expect(current_page).to eq(@author)
+    expect(current_path).to eq(author_path(@author))
     expect(page).to have_content(@author.name)
 
-    within "#author-book-#{book.id}" do
+    within "#author-books" do
       expect(page).to have_content(@book_1.title)
       expect(page).to have_content(@book_1.number_of_pages)
       expect(page).to have_content(@book_1.publish_year)
