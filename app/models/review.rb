38aaa6c4596797d4526d09a4b7bcac644ee8a5ca
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-03-26 19:20:32 -0600

    Adds user pages

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,22 +2,19 @@ class Review < ApplicationRecord
   validates_presence_of :title,
                         :rating,
                         :review_text,
-                        :username
+                        :username,
+                        :created_at
+
   belongs_to :book
 
   def self.top_reviewers
-    reviews = Review.all
-    reviewers_hash = reviews.group_by do |review|
-      review.username
-    end
-    reviewer_count = reviewers_hash.map do |reviewer, reviews|
-      [reviewer, reviews.size]
-    end
-    reviewer_count.sort.first(3)
+    reviews_3 = Review.group(:username).count.take(3)
   end
 
   def self.reviews_by_user
     select(:username)
   end
+
+
 
 end
